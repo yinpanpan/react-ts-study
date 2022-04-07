fa1308a0edea6f0df0cd2228983d13a424af6d1d
铭泉听雪 <mingquantingxue@163.com>
2022-04-08 00:12:16 +0800

    4-8

---
+++ imooc-jira-master/src/utils/http.ts
@@ -0,0 +1,104 @@
+import qs from "qs";
+import * as auth from "auth-provider";
+import { useAuth } from "context/auth-context";
+import { useCallback } from "react";
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
+interface Config extends RequestInit {
+  token?: string;
+  data?: object;
+}
+
+export const http = async (
+  endpoint: string,
+  { data, token, headers, ...customConfig }: Config = {}
+) => {
+  const config = {
+    method: "GET",
+    headers: {
+      Authorization: token ? `Bearer ${token}` : "",
+      "Content-Type": data ? "application/json" : "",
+    },
+    ...customConfig,
+  };
+
+  if (config.method.toUpperCase() === "GET") {
+    endpoint += `?${qs.stringify(data)}`;
+  } else {
+    config.body = JSON.stringify(data || {});
+  }
+
+  // axios 和 fetch 的表现不一样，axios可以直接在返回状态不为2xx的时候抛出异常
+  return window
+    .fetch(`${apiUrl}/${endpoint}`, config)
+    .then(async response => {
+      if (response.status === 401) {
+        await auth.logout();
+        window.location.reload();
+        return Promise.reject({ message: "请重新登录" });
+      }
+      const data = await response.json();
+      if (response.ok) {
+        return data;
+      } else {
+        return Promise.reject(data);
+      }
+    });
+};
+
+// JS 中的typeof，是在runtime时运行的
+// return typeof 1 === 'number'
+
+// TS 中的typeof，是在静态环境运行的
+// return (...[endpoint, config]: Parameters<typeof http>) =>
+export const useHttp = () => {
+  const { user } = useAuth();
+  // utility type 的用法：用泛型给它传入一个其他类型，然后utility type对这个类型进行某种操作
+  return useCallback(
+    (...[endpoint, config]: Parameters<typeof http>) =>
+      http(endpoint, { ...config, token: user?.token }),
+    [user?.token]
+  );
+};
+
+// 类型别名、Utility Type 讲解
+// 联合类型
+let myFavoriteNumber: string | number;
+myFavoriteNumber = "seven";
+myFavoriteNumber = 7;
+// TS2322: Type '{}' is not assignable to type 'string | number'.
+// myFavoriteNumber = {}
+let jackFavoriteNumber: string | number;
+
+// 类型别名在很多情况下可以和interface互换
+// interface Person {
+//   name: string
+// }
+// type Person = { name: string }
+// const xiaoMing: Person = {name: 'xiaoming'}
+
+// 类型别名, interface 在这种情况下没法替代type(联合类型)
+type FavoriteNumber = string | number;
+let roseFavoriteNumber: FavoriteNumber = "6";
+
+// interface 也没法实现Utility type
+type Person = {
+  name: string;
+  age: number;
+};
+const xiaoMing: Partial<Person> = {};//可以不传入类型
+const shenMiRen: Omit<Person, "name" | "age"> = {};//指定某些类型可以不传入
+// keyof获取对象的键值集合
+type PersonKeys = keyof Person;
+// 从类型中挑选几个键值,组成新的类型
+type PersonOnlyName = Pick<Person, "name" | "age">;
+// 过滤联合类型中的某个属性
+// 参数一:联合类型(PersonKeys)
+// 参数二:要删除的属性("name")
+type Age = Exclude<PersonKeys, "name">;
+
+// Partial 的实现
+type Partial<T> = {
+  [P in keyof T]?: T[P];
+};
